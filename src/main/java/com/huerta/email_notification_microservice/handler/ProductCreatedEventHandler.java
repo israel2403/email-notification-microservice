73c9b954bea6c7f6b21d854266d18225b7becd88
Israel Huerta <israelhf24@gmail.com>
2025-03-14 15:10:23 -0600

    Add ProductCreatedEventHandler to handle product creation events from Kafka

---
+++ src/main/java/com/huerta/email_notification_microservice/handler/ProductCreatedEventHandler.java
@@ -0,0 +1,21 @@
+
+package com.huerta.email_notification_microservice.handler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import com.huerta.core.ProductCreatedEvent;
+
+@Component
+public class ProductCreatedEventHandler {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(ProductCreatedEventHandler.class);
+    
+    @KafkaListener(topics = "product-created-events-topic", groupId = "email-notification")
+    public void handleProductCreatedEvent(final ProductCreatedEvent event) {
+
+        LOGGER.info("Product created event received: {}", event.getTitle());
+    }
+}
