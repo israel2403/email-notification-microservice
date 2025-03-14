47cc6558b21819e2cad9f08642ca5f213cb57297
Israel Huerta <israelhf24@gmail.com>
2025-03-14 13:59:42 -0600

    first commit

---
+++ src/test/java/com/huerta/email_notification_microservice/EmailNotificationMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huerta.email_notification_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailNotificationMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
